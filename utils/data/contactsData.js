1bf06d73d6d416d4a7c87d3fd3e29da3a861f104
Akash Adhya <akashadhya19@gmail.com>
2025-02-02 16:47:35 +0530

    some changes

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    email: 'akashadhya19@gmail.com',
+    phone: '+919851671778   ',
+    address: 'Bishnpur, Bankura, West Bengal, India - 722122 ',
+    github: 'https://github.com/Akash-Adhya',
+    facebook: `https://www.facebook.com/profile.php?id=100036571333545`,
+    linkedIn: 'https://www.linkedin.com/in/akash-adhya/',
     twitter: 'https://twitter.com/said7388',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    devUsername: "akash_adhya"
 }
