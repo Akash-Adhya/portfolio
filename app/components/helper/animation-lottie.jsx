6035ed33606ce42f7a28f85dabe9dbbbceab4065
Akash Adhya <akashadhya19@gmail.com>
2025-02-02 15:40:39 +0530

    dynamic import facility in animation-lottie file

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,28 @@
 "use client"
+import dynamic from "next/dynamic";
+// import Lottie from "lottie-react";
 
-import Lottie from "lottie-react";
+// Disable SSR for this component
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationPath,
+  //   style: {
+  //     width: '95%',
+  //   }
+  // };
 
   return (
-    <Lottie {...defaultOptions} />
+    // <Lottie {...defaultOptions} />
+    <Lottie
+      animationData={animationPath}
+      loop={true}
+      autoplay={true}
+      style={{ width: width || "95%" }}
+    />
   );
 };
 
